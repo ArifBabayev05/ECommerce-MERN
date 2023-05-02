4ba619b188e7d454377bac1a31390264c00ec609
Arif Babayev <arifrb@code.edu.az>
2023-05-02 15:39:30 +0400

    Slider finished on home page

--- src/components/ProductCarouselComponent.js
+++ src/components/ProductCarouselComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const ProductCarouselComponent = () => {
     return (
@@ -8,11 +9,14 @@ const ProductCarouselComponent = () => {
                 <img
                     crossorigin="anonymous"
                     className="d-block w-100"
+                    style={{ "height": "300", objectFit: "cover" }}
                     src="/images/carousel/carousel-1.png"
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
+                    <LinkContainer style={{"cursor":"pointer"}} to='/product-detail'>
+                        <h3>Bestsellers in Laptop Category</h3>
+                    </LinkContainer>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
@@ -20,12 +24,15 @@ const ProductCarouselComponent = () => {
                 <img
                     crossorigin="anonymous"
                     className="d-block w-100"
+                    style={{ "height": "300", objectFit: "cover" }}
                     src="/images/carousel/carousel-2.png"
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
+                    <LinkContainer style={{"cursor":"pointer"}} to='/product-detail'>
+                        <h3>Bestsellers in Books Category</h3>
+                    </LinkContainer>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
@@ -33,12 +40,15 @@ const ProductCarouselComponent = () => {
                 <img
                     crossorigin="anonymous"
                     className="d-block w-100"
+                    style={{ "height": "300", objectFit: "cover" }}
                     src="/images/carousel/carousel-3.png"
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <LinkContainer style={{"cursor":"pointer"}} to='/product-detail'>
+                        <h3>Bestsellers in Cameras Category</h3>
+                    </LinkContainer>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                     </p>
--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom"
 const ProductDetailsPage = () =>{
     const {id} = useParams()
     
-    return <p>{id}</p>
+    return <p>sa{id}</p>
 }
 
 export default ProductDetailsPage
