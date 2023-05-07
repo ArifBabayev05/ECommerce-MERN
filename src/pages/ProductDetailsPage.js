787d0f413f20ed861050ee9c80178f2a4ccacb06
Arif Babayev <arifrb@code.edu.az>
2023-05-08 00:36:40 +0400

    Add alert box

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,5 +1,7 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Image, ListGroup, Row, Form, Button, Alert } from "react-bootstrap"
 import { useParams } from "react-router-dom"
+import AddedToCartMessageComponent from "../components/AddedToCardMessageComponent"
+import { Rating } from "react-simple-star-rating"
 
 
 const ProductDetailsPage = () => {
@@ -7,22 +9,80 @@ const ProductDetailsPage = () => {
 
     return (
         <Container>
-            <Row className='mt-5'>
-                <Col size={4}>
-                    Images
+            <AddedToCartMessageComponent />
+
+            <Row className="mt-5">
+                <Col md={4}>
+                    <Image fluid src="/images/games-category.png" />
+                    <Image fluid src="/images/monitors-category.png" />
+                    <Image fluid src="/images/tablets-category.png" />
+                    <Image fluid src="/images/games-category.png" />
                 </Col>
-                <Col size={8}>
+                <Col md={8}>
                     <Row>
-                        <Col size={8}>Product name,Price,Description, Rating</Col>
-                        <Col size={4}>Product status, quantity</Col>
+                        <Col md={8}>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Rating readonly size={20} initialValue={4} /> (1)
+                                </ListGroup.Item>
+                                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col md={4}>
+                            <ListGroup>
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Form.Select size="lg" aria-label="Default select example">
+                                        <option>Open </option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button variant="danger">Danger</Button>
+                                </ListGroup.Item>
+                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
                     </Row>
                     <Row>
                         <Col className="mt-5">
-                            <h5>Reviews</h5>
+                            <h5>REVIEWS</h5>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                            </ListGroup>
                         </Col>
                     </Row>
                     <hr />
-                    send review form
+                    <Alert variant="danger">Login first to write a review</Alert>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                        
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                        <Button variant="primary">Primary</Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
