8e73f80a1bbecaf8887c9a02ae847ebff64e2975
Arif Babayev <arifrb@code.edu.az>
2023-05-11 16:37:52 +0400

    Add zoom function to images

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,22 +1,45 @@
 import { Col, Container, Image, ListGroup, Row, Form, Button, Alert } from "react-bootstrap"
-import { useParams } from "react-router-dom"
+
 import AddedToCartMessageComponent from "../components/AddedToCardMessageComponent"
 import { Rating } from "react-simple-star-rating"
+import ImageZoom from 'js-image-zoom'
+import { useEffect } from "react"
 
 
 const ProductDetailsPage = () => {
-    const { id } = useParams()
+    //Image Zoom Options
+    var options = {
+        scale:2,
+        offset: {vertical: 0, horizontal:10}
+    };
+    useEffect(()=>{
+        new ImageZoom(document.getElementById("first"),options)
+        new ImageZoom(document.getElementById("second"),options)
+        new ImageZoom(document.getElementById("third"),options)
+        new ImageZoom(document.getElementById("fourth"),options)
+    })
 
     return (
         <Container>
             <AddedToCartMessageComponent />
 
             <Row className="mt-5">
-                <Col md={4}>
-                    <Image fluid src="/images/games-category.png" />
-                    <Image fluid src="/images/monitors-category.png" />
-                    <Image fluid src="/images/tablets-category.png" />
-                    <Image fluid src="/images/games-category.png" />
+                <Col style={{zIndex:1}} md={4}>
+                    <div id ="first">
+                        <Image crossOrigin="anonymous" fluid src="/images/games-category.png" />
+                    </div> <br />
+
+                    <div id="second">
+                        <Image crossOrigin="anonymous" fluid src="/images/monitors-category.png" />
+                    </div> <br />
+
+                    <div id="third">
+                        <Image crossOrigin="anonymous" fluid src="/images/tablets-category.png" />
+                    </div> <br />
+
+                    <div id="fourth">
+                        <Image crossOrigin="anonymous" fluid src="/images/games-category.png" />
+                    </div> <br />
                 </Col>
                 <Col md={8}>
                     <Row>
@@ -53,8 +76,13 @@ const ProductDetailsPage = () => {
                         <Col className="mt-5">
                             <h5>REVIEWS</h5>
                             <ListGroup variant="flush">
-                                {Array.from({ length: 10 }).map((item, index) => (
-                                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                {Array.from({ length: 8 }).map((item, index) => (
+                                    <ListGroup.Item key={index}>
+                                        Name of user <br />
+                                        <Rating readonly size={20} initialValue={4} /> <br />
+                                        20-09-2023 <br />
+                                        In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+                                    </ListGroup.Item>
                                 ))}
                             </ListGroup>
                         </Col>
@@ -63,24 +91,20 @@ const ProductDetailsPage = () => {
                     <Alert variant="danger">Login first to write a review</Alert>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="name@example.com" />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
-                            <Form.Label>Example textarea</Form.Label>
+
+                            <Form.Label>Write a review</Form.Label>
                             <Form.Control as="textarea" rows={3} />
                         </Form.Group>
 
                         <Form.Select aria-label="Default select example">
-                            <option>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option>Your Rating</option>
+                            <option value="5">5 (very good)</option>
+                            <option value="4">4 (good)</option>
+                            <option value="3">3 (avarage)</option>
+                            <option value="2">2 (bad)</option>
+                            <option value="1">1 (awful)</option>
                         </Form.Select>
-                        <Button variant="primary">Primary</Button>
+                        <Button className='my-3' variant="primary">Submit</Button>
                     </Form>
                 </Col>
             </Row>
