6b65d45ff474e339ff67fbf58afa0c2dd5ba0f24
Arif Babayev <arifrb@code.edu.az>
2023-05-17 21:49:46 +0400

    User order details page created

--- src/pages/user/UserOrderDetailsPage.js
+++ src/pages/user/UserOrderDetailsPage.js
@@ -1,4 +1,26 @@
-const UserOrderDetailsPage = () =>{
-    return <p>UserOrderDetails Page</p>
+import { Col, Container, Row } from "react-bootstrap"
+
+const UserOrderDetailsPage = () => {
+    return (
+        <Container fluid>
+            <Row className='mt-4'>
+                <h1>Order Details</h1>
+                <Col md={8}>
+                    <br />
+                    <Row>
+                        <Col md={6}>
+                            <h2>Shipping</h2>
+                        </Col>
+                        <Col md={6}>
+                            <h2>Payment method</h2>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <h3>Order Summary</h3>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 export default UserOrderDetailsPage
--- src/pages/user/UserOrderPage.js
+++ src/pages/user/UserOrderPage.js
@@ -1,41 +1,42 @@
 import { Col, Row, Table } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
-const UserOrderPage = () =>{
+const UserOrderPage = () => {
     return (
         <Row className="m-5">
-        <Col md={12}>
-          <h1>My Orders</h1>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
+            <Col md={12}>
+                <h1>My Orders</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>User</th>
+                            <th>Date</th>
+                            <th>Total</th>
+                            <th>Delivered</th>
+                            <th>Order details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {["bi bi-check-lg text-success","bi bi-x-lg text-danger"].map((item,index)=>(
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>Mark Twain</td>
+                            <td>01-01-2023</td>
+                            <td>$135</td>
+                            <td>
+                                <i className={item}></i>
+                            </td>
+                            <td>
+                                <Link to='/user/order-details'>Go to order</Link>
+                            </td>
+                        </tr>
+                        ))}
+
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
     )
 }
 export default UserOrderPage
