4df9574e9d1ea1279d159c0192730f4f2fcfdcf0
Arif Babayev <arifrb@code.edu.az>
2023-05-23 15:29:37 +0400

    Finish UI

--- src/components/filterQueryResultOptions/PriceFilterComponent.js
+++ src/components/filterQueryResultOptions/PriceFilterComponent.js
@@ -4,7 +4,7 @@ const PriceFilterComponent = () => {
   return (
     <>
       <Form.Label>
-        <span className="fw-bold">Price no greater than:</span> 500$
+        <span className="fw-bold">No price greater than:</span> 500$
       </Form.Label>
       <Form.Range min={10} max={1000} step={10} />
     </>
